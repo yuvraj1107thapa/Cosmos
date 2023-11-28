a0fd61ef80be7e0c53f0d3d5e8b4c33844310120
Yuvraj Thapa <yuvrajthapa692@gmail.com>
2023-11-28 19:35:18 +0530

    added search bar functionality

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
+import ClearIcon from '@mui/icons-material/Clear';
 import "./SearchBar.css";
 import { useContext } from "react";
 import { AsideDataContext } from "../../contexts/AsideDataContext";
 import { DataContext } from "../../contexts/DataContext";
+import { NavLink } from "react-router-dom";
 
 const SearchBar = () => {
   const { searchUser, setSearchUser } = useContext(AsideDataContext);
   const { state } = useContext(DataContext);
+
+  const searchResult = searchUser ? (
+    state?.users?.filter(({ username }) =>
+      username.toLowerCase().includes(searchUser.toLowerCase())
+    )
+  ) : (
+    <></>
+  );
 
   return (
     <div className="search-main">
@@ -20,24 +30,35 @@ const SearchBar = () => {
         />
         <div className="search-icon">
           {" "}
-          <SearchIcon />
+         {searchUser ? <div id="clearBtn" onClick={()=>setSearchUser("")}> <ClearIcon /> </div> : <SearchIcon />}
         </div>
       </div>
       {searchUser && (
         <div className="dataResults">
-          {state.users.map((ele, key) => {
+          {searchResult.length === 0 ? <h3 className="nav-profile">No results found</h3> :
+          searchResult.map((ele) => {
             return (
-              <a
-                className="dataItem"
-                href={ele.link}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <p>{ele.username}</p>
-              </a>
+              <NavLink to={`/profilepage/${ele.username}`}>
+                {/* <p>{ele.username}</p> */}
+                <div className="nav-profile">
+                  <img
+                    src={ele?.avatarUrl}
+                    alt=""
+                    className="nav-profile-pic"
+                  />
+                  <div>
+                    <h4>
+                      {ele.firstName} {ele.lastName}
+                    </h4>
+                    <span>@{ele.username}</span>
+                  </div>
+                </div>
+              </NavLink>
             );
           })}
-          {state.users.length === 0 && <p>No results found</p>}
+          {/* <div className="dataResults">
+          {searchResult.length === 0 && <h5>No results found</h5>}
+          </div> */}
         </div>
       )}
     </div>
