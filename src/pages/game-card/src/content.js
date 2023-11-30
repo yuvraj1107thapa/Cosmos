74625a9de858ac0113cd0249b9b4de94c2ca903f
Adarsh Negi <adarshn8487@gmail.com>
2023-11-30 19:54:55 +0530

    Added game place

---
+++ src/pages/game-card/src/content.js
@@ -0,0 +1,70 @@
+const productData = [
+    {
+      id: 1,
+      name: "Floppy Bird",
+      image:
+        "https://github.com/NegiAdarsh/story2/assets/100505819/080b4b10-d944-4e13-8ef2-f65076123792",
+     
+      
+      rating: 5,
+      url:"https://negiadarsh.github.io/floppy/",
+      totalSales: 7479 
+    },
+    {
+      id: 2,
+      name: "Simon",
+      image:
+        "https://github.com/NegiAdarsh/story2/assets/100505819/bde53987-5c75-4d43-b5a7-0ce5be7a270b",
+        url:"https://negiadarsh.github.io/simon/",
+      
+      rating: 5,
+     
+      totalSales: 6898
+    },
+    {
+      id: 3,  
+      name: "Card-Flip",
+      image:
+        "https://github.com/NegiAdarsh/story2/assets/100505819/2cddac8d-4373-4771-b01a-d3b03c88b2a4",
+     
+        url:"https://negiadarsh.github.io/memory/",
+      rating: 4,
+      
+      totalSales: 6347 
+    },
+    {
+      id: 4,
+      name: "Wordle",
+      image:
+        "https://github.com/NegiAdarsh/story2/assets/100505819/2cddac8d-4373-4771-b01a-d3b03c88b2a4",
+        url:"",
+      
+      rating: 5,
+      
+      totalSales: 7479 
+    },
+    {
+      id: 5,
+      name: "Ludo",
+      image:
+        "https://github.com/NegiAdarsh/story2/assets/100505819/8caf6fe1-56fd-402e-8838-42c184c3f4ff",
+   
+      url:"https://negiadarsh.github.io/pac-man/",
+      rating: 2,
+      
+      totalSales: 234
+    },
+    {
+      id: 6,
+      name: "Pac-Man",
+      image:
+        "https://github.com/NegiAdarsh/story2/assets/100505819/8e382868-d797-463a-8a92-c99a6839393c",
+  
+        url:"https://negiadarsh.github.io/pac-man/",
+      rating: 10,
+      
+      totalSales: 74791
+    }
+    
+  ];
+  export default productData;
